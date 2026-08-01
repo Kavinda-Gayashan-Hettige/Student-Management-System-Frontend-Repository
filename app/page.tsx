7850b4f1502a14c26b3a7b8fa364ba8e3a136862
kavinda1996 <gayashankavinda95@gmail.com>
2026-08-01 15:12:40 +0530

    feat(home): update homepage design and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="min-h-screen bg-cover bg-center bg-no-repeat relative text-zinc-50"
+      style={{ backgroundImage: "url('/home.png')" }}>
       <h1 className="text-3xl font-bold">Student Management System - Home</h1>
     </div>
   );
