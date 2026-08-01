ab7f7d2e1627399709d9b636a6ac7832dcc9b508
kavinda1996 <gayashankavinda95@gmail.com>
2026-08-01 15:15:20 +0530

    refactor(api): update API service integration

--- app/services/api.js
+++ app/services/api.js
@@ -1,21 +1,42 @@
 import axios from 'axios';
 
+
 const API_BASE_URL = 'http://localhost:8080/api';
 
-// Student APIs
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+});
+
+
+api.interceptors.request.use(
+  (config) => {
+  
+    if (typeof window !== 'undefined') {
+      const token = localStorage.getItem('token');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+
 export const getStudents = async () => {
-    const response = await axios.get(`${API_BASE_URL}/students`);
-    return response.data;
+  const response = await api.get('/students');
+  return response.data;
 };
 
-// Course APIs
 export const getCourses = async () => {
-    const response = await axios.get(`${API_BASE_URL}/courses`);
-    return response.data;
+  const response = await api.get('/courses');
+  return response.data;
 };
 
-// User APIs
 export const getUsers = async () => {
-    const response = await axios.get(`${API_BASE_URL}/users`);
-    return response.data;
+  const response = await api.get('/users');
+  return response.data;
 };
